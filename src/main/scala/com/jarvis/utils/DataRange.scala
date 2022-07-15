624f4cca912a3ed585905daf3ad2f04215500339
Denton Cockburn <kanielc@gmail.com>
2022-07-15 14:31:24 -0400

    add tests for datarange

--- src/main/scala/com/jarvis/utils/DataRange.scala
+++ src/main/scala/com/jarvis/utils/DataRange.scala
@@ -4,15 +4,20 @@ import org.apache.spark.sql.Column
 import org.apache.spark.sql.functions.expr
 
 import java.sql._
+import java.text.SimpleDateFormat
 
 /** Filters a column by an inclusive range. Meant to be equivalent to ANSI SQL's BETWEEN operator */
 case class DataRange[T: Ordering](private val column: String, private val start: T, private val end: T) {
   require(column != null && start != null && end != null, s"Null parameters are not allowed for DateRange.  Parameters provided are column: $column, start: $start, end: $end")
   require(implicitly[Ordering[T]].compare(start, end) < 1, s"parameter $start must <= $end")
 
+  def date2String(d: java.util.Date) = new SimpleDateFormat("yyyy-MM-dd").format(d)
+
   val queryString: String = start match {
-    case x: AnyRef if (x.isInstanceOf[String] || x.isInstanceOf[Date] || x.isInstanceOf[Timestamp]) && start == end => s"($column = '$start')"
-    case x: AnyRef if x.isInstanceOf[String] || x.isInstanceOf[Date] || x.isInstanceOf[Timestamp] => s"($column BETWEEN '$start' and '$end')"
+    case x: AnyRef if (x.isInstanceOf[String] || x.isInstanceOf[Timestamp]) && start == end => s"($column = '$start')"
+    case x: AnyRef if (x.isInstanceOf[String] || x.isInstanceOf[Timestamp]) => s"($column BETWEEN '$start' and '$end')"
+    case x: AnyRef if x.isInstanceOf[java.util.Date] && start == end => s"($column = '${date2String(start.asInstanceOf[java.util.Date])}')"
+    case x: AnyRef if x.isInstanceOf[java.util.Date] => s"($column BETWEEN '${date2String(start.asInstanceOf[java.util.Date])}' and '${date2String(end.asInstanceOf[java.util.Date])}')"
     case _: java.lang.Number if start == end => s"($column = $start)"
     case _: java.lang.Number => s"($column BETWEEN $start and $end)"
   }
--- src/test/scala/com/jarvis/utils/DataRangeTest.scala
+++ src/test/scala/com/jarvis/utils/DataRangeTest.scala
@@ -1,5 +1,11 @@
 package com.jarvis.utils
 
+import java.sql.Timestamp
+import java.lang.IllegalArgumentException
+import DataRange._
+
+import java.text.SimpleDateFormat
+
 class DataRangeTest extends SparkTest {
   test("datarange works for int") {
     // This will also test the apply method
@@ -7,5 +13,40 @@ class DataRangeTest extends SparkTest {
 
     DataRange("foo", 7, 9).queryString shouldEqual "(foo BETWEEN 7 and 9)"
   }
+
+  test("datarange works for string date") {
+    // This will also test the apply method
+    DataRange("foo", "2020-01-02").queryString shouldEqual "(foo = '2020-01-02')"
+
+    DataRange("foo", "2020-01-02", "2020-12-31").queryString shouldEqual "(foo BETWEEN '2020-01-02' and '2020-12-31')"
+  }
+
+  test("datarange works for date objects") {
+    val df = new SimpleDateFormat("yyyy-MM-dd")
+    val start = df.parse("2020-01-02")
+    val end = df.parse("2020-12-31")
+    DataRange("foo", start).queryString shouldEqual "(foo = '2020-01-02')"
+
+    DataRange("foo", start, end).queryString shouldEqual "(foo BETWEEN '2020-01-02' and '2020-12-31')"
+  }
+
+  test("datarange works for timestamps") {
+    val start = new Timestamp(1577923200000L)
+    val end = new Timestamp(1609376117000L)
+    DataRange("foo", start).queryString shouldEqual "(foo = '2020-01-02 00:00:00.0')"
+
+    DataRange("foo", start, end).queryString shouldEqual "(foo BETWEEN '2020-01-02 00:00:00.0' and '2020-12-31 00:55:17.0')"
+  }
+
+  test("datarange null tests") {
+    an [IllegalArgumentException] should be thrownBy DataRange(null, 7)
+    an [IllegalArgumentException] should be thrownBy DataRange("foo", null)
+    an [IllegalArgumentException] should be thrownBy DataRange("foo", new Integer(7), null)
+  }
+
+  test("datarange fails if ordering is wrong") {
+    an [IllegalArgumentException] should be thrownBy DataRange("foo", 9, 7)
+  }
+
 
 }
